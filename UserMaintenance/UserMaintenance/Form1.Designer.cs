7131bfa4a9776c5d9adc43218488785b4aa811d6
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-09-30 11:15:37 +0200

    9. Feladat

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -31,10 +31,9 @@ namespace UserMaintenance
         {
             this.listUsers = new System.Windows.Forms.ListBox();
             this.txtLastName = new System.Windows.Forms.TextBox();
-            this.txtFirstName = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.lblLastName = new System.Windows.Forms.Label();
-            this.lblFirstName = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -52,16 +51,9 @@ namespace UserMaintenance
             this.txtLastName.Size = new System.Drawing.Size(119, 20);
             this.txtLastName.TabIndex = 1;
             // 
-            // txtFirstName
-            // 
-            this.txtFirstName.Location = new System.Drawing.Point(400, 42);
-            this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(119, 20);
-            this.txtFirstName.TabIndex = 2;
-            // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(325, 68);
+            this.btnAdd.Location = new System.Drawing.Point(325, 42);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(194, 23);
             this.btnAdd.TabIndex = 3;
@@ -78,24 +70,24 @@ namespace UserMaintenance
             this.lblLastName.TabIndex = 4;
             this.lblLastName.Text = "label1";
             // 
-            // lblFirstName
+            // btnWrite
             // 
-            this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(322, 42);
-            this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(35, 13);
-            this.lblFirstName.TabIndex = 5;
-            this.lblFirstName.Text = "label2";
+            this.btnWrite.Location = new System.Drawing.Point(325, 71);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(194, 23);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "button1";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.btnWrite);
             this.Controls.Add(this.lblLastName);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.listUsers);
             this.Name = "Form1";
@@ -109,10 +101,9 @@ namespace UserMaintenance
 
         private System.Windows.Forms.ListBox listUsers;
         private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.TextBox txtFirstName;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label lblLastName;
-        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Button btnWrite;
     }
 }
 
