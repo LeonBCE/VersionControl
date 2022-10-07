002ecaf14911aea1b479267dafa2977ba0cde23b
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-10-07 11:07:35 +0200

    7. Feladat

--- UserMaintenance/IRFGyak04/IRFGyak04/Form1.cs
+++ UserMaintenance/IRFGyak04/IRFGyak04/Form1.cs
@@ -15,9 +15,8 @@ namespace IRFGyak04
     public partial class Form1 : Form
     {
 
-        List<Flat> Flats;
-
         RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
 
         Excel.Application xlApp;
         Excel.Workbook xlWb;
@@ -38,18 +37,18 @@ namespace IRFGyak04
             {
                 xlApp = new Excel.Application();
 
-                xlWb = new Excel.Workbook();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
 
-                xlSheet = new Excel.Worksheet();
+                xlSheet = xlWb.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
             catch (Exception ex)
             {
-                string errMsg = string.Format("Error: {0}\nLine {1}", ex.Message, ex.Source);
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
 
                 xlWb.Close(false, Type.Missing, Type.Missing);
@@ -63,11 +62,72 @@ namespace IRFGyak04
 
         private void CreateTable()
         {
+            string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length ; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.District.ToString() == "true")
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
 
         private void LoadData()
         {
-            List<Flat> Flats = context.Flat.ToList();
+            Flats = context.Flat.ToList();
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
