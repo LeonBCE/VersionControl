7ba614b955e30d36ae61a0e1602d85ac68269392
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-10-07 10:27:45 +0200

    6. Feladat

--- UserMaintenance/IRFGyak04/IRFGyak04/Form1.cs
+++ UserMaintenance/IRFGyak04/IRFGyak04/Form1.cs
@@ -19,11 +19,50 @@ namespace IRFGyak04
 
         RealEstateEntities context = new RealEstateEntities();
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
 
             LoadData();
+
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWb = new Excel.Workbook();
+
+                xlSheet = new Excel.Worksheet();
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+
+            }
+
+        }
+
+        private void CreateTable()
+        {
         }
 
         private void LoadData()
