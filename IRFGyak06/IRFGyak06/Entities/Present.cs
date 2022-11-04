008f850dc702bc0909a7b1e4a571391545821592
LeonBCE <fabo.tompos@stud.uni-corvinus.hu>
2022-11-04 19:00:16 +0100

    13 2/2

--- IRFGyak06/IRFGyak06/Entities/Present.cs
+++ IRFGyak06/IRFGyak06/Entities/Present.cs
@@ -10,17 +10,20 @@ namespace IRFGyak06.Entities
 {
     class Present : Toy
     {
-        public Color RibbonColor { get; set; }
-        public Color BoxColor { get; set; }
+        public SolidBrush RibbonColor { get; private set; }
+        public SolidBrush BoxColor { get; private set; }
 
-        public Present(Color color)
+        public Present(Color bcolor, Color rcolor)
         {
-            //BoxColor = new SolidBrush(color);
+            BoxColor = new SolidBrush(bcolor);
+            RibbonColor = new SolidBrush(rcolor);
         }
 
         protected override void DrawImage(Graphics g)
         {   
-            //g.FillRectangle(BoxColor, 0, 0, Width, Height);
+            g.FillRectangle(BoxColor, 0, 0, Width, Height);
+            g.FillRectangle(RibbonColor, Width/2-(Width/5/2), 0, Width/5, Height);
+            g.FillRectangle(RibbonColor, 0, Height/2-(Height/5/2) , Width, Height/5);
         }
 
     }
