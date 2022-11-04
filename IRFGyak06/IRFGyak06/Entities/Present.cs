1c44c0b61b06ae698f4cb9a386e8ed1de4ff50ed
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-11-04 11:16:32 +0100

    13. Feladat 1/2

---
+++ IRFGyak06/IRFGyak06/Entities/Present.cs
@@ -0,0 +1,27 @@
+﻿using IRFGyak06.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRFGyak06.Entities
+{
+    class Present : Toy
+    {
+        public Color RibbonColor { get; set; }
+        public Color BoxColor { get; set; }
+
+        public Present(Color color)
+        {
+            //BoxColor = new SolidBrush(color);
+        }
+
+        protected override void DrawImage(Graphics g)
+        {   
+            //g.FillRectangle(BoxColor, 0, 0, Width, Height);
+        }
+
+    }
+}
