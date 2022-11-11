9e7ca358515808f1d44aa2a23732850538439b6a
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-11-11 11:02:54 +0100

    9. Feladat

--- IRFGyak07/IRFGyak07/Form1.cs
+++ IRFGyak07/IRFGyak07/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace IRFGyak07
     public partial class Form1 : Form
     {
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
+        List<decimal> NyereségekOrdered = new List<decimal>();
 
         PortfolioEntities context = new PortfolioEntities();
 
@@ -28,6 +31,47 @@ namespace IRFGyak07
 
             CreatePortfolio();
 
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
+            NyereségekOrdered = Nyereségek;
+        }
+
+        private void SavePortfolio()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files | *.txt";
+            sfd.DefaultExt = "txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    sw.WriteLine("Időszak;Nyereség");
+                    int counter = 0;
+                    foreach (var v in NyereségekOrdered)
+                    {
+                        sw.WriteLine(counter + ";" + v);
+                        counter++;
+                    }
+                }
+            }
         }
 
         private void CreatePortfolio()
@@ -52,6 +96,11 @@ namespace IRFGyak07
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SavePortfolio();
         }
     }
 }
