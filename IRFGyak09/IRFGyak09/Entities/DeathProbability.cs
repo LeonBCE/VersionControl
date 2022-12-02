cf649f48cf8217b5b4fdedab760ac45251cd8451
Tompos Fabó Leon <fabo.tompos@stud.uni-corvinus.hu>
2022-12-02 10:15:54 +0100

    2. Feladat

---
+++ IRFGyak09/IRFGyak09/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRFGyak09.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
+
+        public double deathProbability { get; set; }
+    }
+}
